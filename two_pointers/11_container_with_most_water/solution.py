d554f5c4e6febe4bcafb66513753921e1d671fba
Nguyễn Thị Hải Yến <71604562+yen102@users.noreply.github.com>
2023-01-27 17:43:51 +0700

    finish problem 11

--- two_pointers/11_container_with_most_water/solution.py
+++ two_pointers/11_container_with_most_water/solution.py
@@ -1,2 +1,18 @@
 class Solution(object):
-  pass
+  def maxArea(self, height):
+    """
+    :type height: List[int]
+    :rtype: int
+    """
+    left = 0
+    right = len(height) - 1
+
+    res = 0
+    while(left < right):
+      area = (right - left) * min(height[right], height[left])
+      res = max(res, area)
+      if (height[left] <= height[right]):
+        left += 1
+      else:
+        right -= 1
+    return res
--- two_pointers/11_container_with_most_water/test_solution.py
+++ two_pointers/11_container_with_most_water/test_solution.py
@@ -1,7 +1,14 @@
+from solution import Solution
 class TestSolution(object):
-  def maxArea(self, height):
-    """
-    :type height: List[int]
-    :rtype: int
-    """
-    
+  sol = Solution()
+  def test_1(self):
+    height = [1,8,6,2,5,4,8,3,7]
+    assert self.sol.maxArea(height) == 49
+
+  def test_2(self):
+    height = [8,6,2,5,4,8,3,7]
+    assert self.sol.maxArea(height) == 49
+  
+  def test_3(self):
+    height = [8,6]
+    assert self.sol.maxArea(height) == 6
